2178ffd3aa95b63051eb6935657e4aa629e3bebc
My Name <cpgsom819@gmail.com>
2023-05-24 19:36:43 +0800

    adding resposive css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,22 @@
 /*=============== SHOW MENU ===============*/
 
+const navMenu = document.getElementById('nav__menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+   
+      if(navToggle) {
+        navToggle.addEventListener("click", () => {
+          navMenu.classList.add('show-menu')
+        })
+      }
+
+      if(navClose) {
+        navClose.addEventListener("click", () => {
+          navMenu.classList.remove('show-menu')
+        })
+      }
+
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
