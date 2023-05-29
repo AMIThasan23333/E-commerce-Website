c0865a5af1c432357dcb83e7a387618cbde82596
My Name <cpgsom819@gmail.com>
2023-05-29 08:23:13 +0800

    adding lightbox

--- assets/js/main.js
+++ assets/js/main.js
@@ -147,17 +147,19 @@ window.addEventListener('scroll',scrollUp)
 
 /*=============== QUESTIONS ACCORDION ===============*/
 
-const accordionItem = document.querySelectorAll('.questions__item')
+  const accordionItem = document.querySelectorAll('.questions__item')
 
-accordionItem.forEach((item) => {
+  accordionItem.forEach((item) => {
 
   const accordionHeader = item.querySelector('.questions__header')
 
-  accordionHeader.addEventListener('click', () => {
 
+  accordionHeader.addEventListener('click', () => {
     const openItem = document.querySelector('.accordion-open')
-     
     toggleItem(item)
+    if(openItem && openItem !==item ){
+      toggleItem(openItem)
+    }
   })
 
 })
@@ -166,17 +168,10 @@ accordionItem.forEach((item) => {
  const toggleItem = (item) => {
 
   const accordionContent  = item.querySelector('.questions__content')
-
-
   if(item.classList.contains('accordion-open')){
-
     accordionContent.removeAttribute('style')
     item.classList.remove('accordion-open')
-
-
   }
-
-  
      else  {
       accordionContent.style.height = accordionContent.scrollHeight + 'px';
       item.classList.add('accordion-open')
